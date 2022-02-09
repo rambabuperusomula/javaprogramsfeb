790424e38a52c6795d5da8737864bc3d36f09e39
Babu <rambabu.p@audreeinfotech.com>
2022-02-09 20:28:56 +0530

    conditions

---
+++ passfail.java
@@ -0,0 +1,32 @@
+public class passfail {
+	
+	public static void main(String[] args) {
+			
+		int m=66;
+		if(m<35)
+		{
+			System.out.println("fail");			
+		}
+		 if(m>=35)
+		{
+			System.out.println("pass");
+		}
+		if(m>=50)
+		{
+			System.out.println("third class");			
+		}
+		if(m>=60)
+		{
+			System.out.println("second class");			
+		}
+		if(m>=70)
+		{
+			System.out.println("first class");			
+		}
+		if(m>=90)
+		{
+			System.out.println("Distrinction");			
+		}	
+	}	
+
+}
