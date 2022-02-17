7643eaf46d4d960965e8e7705ddf8bc12ea1fd62
Babu <rambabu.p@audreeinfotech.com>
2022-02-17 17:24:42 +0530

    encapsulation

---
+++ Encapsulation.java
@@ -0,0 +1,15 @@
+public class Encapsulation {
+	
+private	int i;
+	
+public int getm()
+{
+	return i;
+}
+public void setm(int i)
+{
+	this.i=i;	
+}
+
+
+}
---
+++ EncapsulationRunner .java
@@ -0,0 +1,8 @@
+public class EncapsulationRunner {
+
+	public static void main(String args[])
+	{
+		Encapsulation e=new Encapsulation();
+		System.out.println(e.getm());
+	}
+}
